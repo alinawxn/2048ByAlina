d2e145c0906f213b54c37ccd889c3edf357c8ea6
alinawxn <alinawxn@gmail.com>
2014-04-16 15:44:27 -0400

    successful logic

--- JS/2048.js
+++ JS/2048.js
@@ -4,12 +4,13 @@ var startFlag = 0;
 
 $(document).keydown(function(event){
 	Mmove(event.which);
-	Generate2Or4();
-	drawTable();
+	if(event.which == 37 || event.which == 38 || event.which == 39 || event.which == 40){
+		Generate2Or4();
+		drawTable();
+	}
 });
 
 function drawTable(){
-	var MOld = M;
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
 			if(M[i][j] == 0)
@@ -18,7 +19,6 @@ function drawTable(){
 	}
 	var htmlStr="<table border = '1' width = '400' height = '400' align='center'><tr><td width = '100' height = '100'>"+M[0][0]+"</td><td width = '100' height = '100'>"+M[0][1]+"</td><td width = '100' height = '100'>"+M[0][2]+"</td><td width = '100' height = '100'>"+M[0][3]+"</td></tr><tr><td width = '100' height = '100'>"+M[1][0]+"</td><td width = '100' height = '100'>"+M[1][1]+"</td><td width = '100' height = '100'>"+M[1][2]+"</td><td width = '100' height = '100'>"+M[1][3]+"</td></tr><tr><td width = '100' height = '100'>"+M[2][0]+"</td><td width = '100' height = '100'>"+M[2][1]+"</td><td width = '100' height = '100'>"+M[2][2]+"</td><td width = '100' height = '100'>"+M[2][3]+"</td></tr><tr><td width = '100' height = '100'>"+M[3][0]+"</td><td width = '100' height = '100'>"+M[3][1]+"</td><td width = '100' height = '100'>"+M[3][2]+"</td><td width = '100' height = '100'>"+M[3][3]+"</td></tr></table>";
 	$('#table').html(htmlStr);
-	M = MOld;
 }
 
 function Generate2Or4(){
@@ -31,28 +31,62 @@ function Generate2Or4(){
 		twoOrFour = 4;
 	}
 	var amountOfZero = 0;
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4; j++) {
-			if (M[i][j] == 0) {
+	var N = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]];
+	for(i = 0; i < 4; i++){
+		for(j = 0; j < 4; j++){
+			if(M[i][j] == 0){
+				N[amountOfZero][0] = i;
+				N[amountOfZero][1] = j;
 				amountOfZero++;
 			}
 		}
 	}
-	var RandomNumber = Math.floor(Math.random()*amountOfZero);
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4; j++) {
-			if (M[i][j] == 0) {
-				RandomNumber--;
-				if (RandomNumber == 0) {
-					M[i][j] = twoOrFour;
-				}
-			}
-		}
-	}
+	var randomNumber = Math.floor(Math.random()*amountOfZero);
+	M[N[randomNumber][0]][N[randomNumber][1]] = twoOrFour;
 }
 
 function Mmove(d){
 	switch(d){
+		case 38:
+		var N = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+		for(j = 0; j < 4; j++){
+			var n = 0
+			for(i = 0; i<4 ; i++){
+				if(M[i][j] != 0){
+					N[n][j] = M [i][j];
+					n++;
+				}
+			}
+			//alert("1");
+			//alert(N[0][0]+N[0][1]+N[0][2]+N[0][3]+N[0][0]+N[1][1]+N[1][2]+N[1][3]+N[2][0]+N[2][1]+N[2][2]+N[2][3]+N[3][0]+N[3][1]+N[3][2]+N[3][3]+);
+			for(i = 0; i< 4; i++){
+				M[i][j] = N[i][j];
+			}
+			//alert("2");
+			for(i = 0; i < 3; i++){
+				if((M[i][j]!=0) && (M[i][j]==M[i+1][j])) {
+					M[i][j] = 2*M[i][j];
+					M[i+1][j] = 0;
+				}
+			}
+			//alert("3");
+			n = 0;
+			N = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+			for(i = 0; i<4 ; i++){
+				if(M[i][j] != 0){
+					N[n][j] = M [i][j];
+					n++;
+				}
+			}
+			//alert("4");
+			for(i = 0; i< 4; i++){
+				M[i][j] = N[i][j];
+				if(N[i][j] != 0){
+				}
+			}
+			//alert("5");
+		}
+		break;
 		case 37:
 		var N = [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]];
 		for(i = 0; i < 4; i++){
@@ -66,7 +100,7 @@ function Mmove(d){
 			for(j = 0; j < 4; j++){
 				M[i][j] = N[i][j];
 			}
-			for(j = 0; j < 4; j++){
+			for(j = 0; j < 3; j++){
 				if(M[i][j]!=0 && M[i][j]==M[i][j+1]){
 					M[i][j] = 2 * M[i][j];
 					M[i][j+1] = 0;
@@ -85,38 +119,6 @@ function Mmove(d){
 			}
 		}
 		break;
-		case 38:
-		var N = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
-		for(j = 0; j < 4; j++){
-			var n = 0
-			for(i = 0; i<4 ; i++){
-				if(M[i][j] != 0){
-					N[n][j] = M [i][j];
-					n++;
-				}
-			}
-			for(i = 0; i< 4; i++){
-				M[i][j] = N[i][j];
-			}
-			for(i = 0; i < 4; i++){
-				if(M[i][j]!=0 && M[i][j] == M[i+1][j]){
-					M[i][j] = 2*M[i][j];
-					M[i+1][j] = 0;
-				}
-			}
-			n = 0;
-			N = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
-			for(i = 0; i<4 ; i++){
-				if(M[i][j] != 0){
-					N[n][j] = M [i][j];
-					n++;
-				}
-			}
-			for(i = 0; i< 4; i++){
-				M[i][j] = N[i][j];
-			}
-		}
-		break;
 		case 39:
 		var N = [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]];
 		for(i = 3; i > -1; i--){
@@ -130,7 +132,7 @@ function Mmove(d){
 			for(j = 3; j > -1; j--){
 				M[i][j] = N[i][j];
 			}
-			for(j = 3; j > -1; j--){
+			for(j = 3; j > 0; j--){
 				if(M[i][j]!=0 && M[i][j]==M[i][j-1]){
 					M[i][j] = 2 * M[i][j];
 					M[i][j-1] = 0;
@@ -162,7 +164,7 @@ function Mmove(d){
 			for(i = 3; i > -1; i--){
 				M[i][j] = N[i][j];
 			}
-			for(i = 3; i > -1; i--){
+			for(i = 3; i > 0; i--){
 				if(M[i][j]!=0 && M[i][j]==M[i-1][j]){
 					M[i][j] = 2 * M[i][j];
 					M[i-1][j] = 0;
@@ -180,6 +182,8 @@ function Mmove(d){
 				M[i][j] = N[i][j];
 			}
 		}
+		break;
+		default:
 		break;
 	}
 }
